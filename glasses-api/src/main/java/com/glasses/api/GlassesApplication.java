ad20fa7b422bb43d4b329d9270b9851837fdd8e9
莫单 <modan@singularitychina.com>
2019-11-07 14:36:17 +0800

    init

---
+++ glasses-api/src/main/java/com/glasses/api/GlassesApplication.java
@@ -0,0 +1,17 @@
+package com.glasses.api;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description : null.java
+ * @auther : modan
+ * @create : 2019-11-07 11:57
+ */
+@SpringBootApplication
+@Configuration
+public class GlassesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GlassesApplication.class, args);
+    }
+}
